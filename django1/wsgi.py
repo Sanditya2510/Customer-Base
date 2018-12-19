2713cf7b87d29e6082d79409b934893d02ed584d
Sanditya Srivastava <sanditya2510@gmail.com>
2018-12-19 21:14:55 +0530

    first commit after deployment

---
+++ django1/wsgi.py
@@ -0,0 +1,8 @@
+from dj_static import Cling
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django1.settings')
+
+application = Cling(get_wsgi_application())
---
+++ djangoapp1/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from djangoapp1.models import *
+# Register your models here.
+admin.site.register(Profession)
+admin.site.register(DataSheet)
+admin.site.register(Customer)
+admin.site.register(Document)
